2f34b5156f41f6c6c32c1c12712d0c821523f12a
Junxiang Wen <tuk61730@temple.edu>
2020-02-08 10:22:39 -0500

    Assignment 2 Revised. add comments.

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -1,6 +1,7 @@
 package edu.temple.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,42 +10,43 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
-   // private static final String TAG ="aa";
+    // private static final String TAG ="aa";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button =  findViewById(R.id.button);
+        //get button by ID
+        Button button = findViewById(R.id.button);
 
-
-
+        // add a listener for button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String st =  ((EditText)findViewById(R.id.name)).getText().toString();
-                String st2 =  ((EditText)findViewById(R.id.user_e)).getText().toString();
-                String st3 =  ((EditText)findViewById(R.id.password)).getText().toString();
-                String st4 =  ((EditText)findViewById(R.id.confirmation)).getText().toString();
-                if(st.equals("")||st2.equals("")||st3.equals("")||st4.equals("")){
+                //get string for 4 edit text
+                String st = ((EditText) findViewById(R.id.name)).getText().toString();
+                String st2 = ((EditText) findViewById(R.id.user_e)).getText().toString();
+                String st3 = ((EditText) findViewById(R.id.password)).getText().toString();
+                String st4 = ((EditText) findViewById(R.id.confirmation)).getText().toString();
+                
+                if (st.equals("") || st2.equals("") || st3.equals("") || st4.equals("")) {
                     //Log.d(TAG,"You must enter all information");
-                    Toast.makeText(getApplicationContext(),"You must enter all information",Toast.LENGTH_LONG).show();
-                }else if(!st3.equals(st4)){
+                    Toast.makeText(getApplicationContext(), "You must enter all information", Toast.LENGTH_LONG).show();
+                } else if (!st3.equals(st4)) {
                     //Log.d(TAG,"Passwords do not match, try again");
-                    Toast.makeText(getApplicationContext(),"Passwords do not match, try again",Toast.LENGTH_LONG).show();
-                }else{
-                    ((EditText)findViewById(R.id.name)).setText("");
-                    ((EditText)findViewById(R.id.user_e)).setText("");
-                    ((EditText)findViewById(R.id.password)).setText("");
-                    ((EditText)findViewById(R.id.confirmation)).setText("");
-                    Toast.makeText(getApplicationContext(),"Welcome, "+ st +" to the SignUpForm App",Toast.LENGTH_LONG).show();
-                   // Log.d(TAG,"666");
+                    Toast.makeText(getApplicationContext(), "Passwords do not match, try again", Toast.LENGTH_LONG).show();
+                } else {
+                    ((EditText) findViewById(R.id.name)).setText("");
+                    ((EditText) findViewById(R.id.user_e)).setText("");
+                    ((EditText) findViewById(R.id.password)).setText("");
+                    ((EditText) findViewById(R.id.confirmation)).setText("");
+                    Toast.makeText(getApplicationContext(), "Welcome, " + st + " to the SignUpForm App", Toast.LENGTH_LONG).show();
+                    // Log.d(TAG,"666");
 
                 }
             }
         });
     }
-
 
 
 }
